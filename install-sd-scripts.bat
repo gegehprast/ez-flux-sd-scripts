922e7a40f55777f68e5e0af2bcb62c6b27394eee
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-03-09 00:25:27 +0700

    update install script to use specific versions of torch and torchvision, and adjust CUDA index URL

--- install-sd-scripts.bat
+++ install-sd-scripts.bat
@@ -3,12 +3,14 @@ git clone -b sd3 https://github.com/kohya-ss/sd-scripts
 python -m venv venv
 .\venv\Scripts\activate
 
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu126
+pip3 install torch==2.4.0 torchvision==0.19.0 --index-url https://download.pytorch.org/whl/cu124
 
 cd sd-scripts
 pip install --upgrade -r requirements.txt
 
 cd ..
-pip install xformers --index-url https://download.pytorch.org/whl/cu126
+pip install xformers --index-url https://download.pytorch.org/whl/cu124
 
 accelerate config
+
+sudo apt-get update && sudo apt-get install libgl1
